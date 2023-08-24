14dd16e25d1151608c4c7a8ba4e4eeedf1996460
Brendan Heywood <brendan@catalyst-au.net>
2023-08-24 14:26:10 +1000

    Moved the intercept to a new class

--- lib/filestorage/file_storage.php
+++ lib/filestorage/file_storage.php
@@ -467,9 +467,8 @@ class file_storage {
         } else if ($mode === 'bigthumb') {
             $data = $file->generate_image_thumbnail(250, 250);
         } else if (substr($mode, 0, 9) == 'transform') {
-print "image";
-die;
-
+            $transform = new \core\media\image_transform();
+            $data = $transform->get_final_image();
         } else {
             throw new file_exception('storedfileproblem', 'Invalid preview mode requested');
         }
