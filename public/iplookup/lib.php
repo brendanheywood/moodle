8ae5eb6281a357b8fb5f3afeada5d3c559428475
Brendan Heywood <brendan@catalyst-au.net>
2026-01-20 00:04:36 +1100

    MDL-87697 iplookup: Add region and translated location names
    This adds a new field fo region, and where possible translates
    the city, region and country fields. This also remove the title
    which is display logic and should be up to the use of the api.

--- public/iplookup/lib.php
+++ public/iplookup/lib.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -21,51 +20,95 @@
  * @package    core
  * @subpackage iplookup
  * @copyright  2010 Petr Skoda {@link http://skodak.org}
+ * @copyright  2026 Brendan Heywood <brendan@catalyst-au.net>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Returns location information
+ *
+ * If possible returns names in the language passed in.
+ *
  * @param string $ip
+ * @param string $lang
  * @return array
  */
-function iplookup_find_location($ip) {
+function iplookup_find_location(string $ip, string $lang = '') {
     global $CFG;
 
-    $info = array('city'=>null, 'country'=>null, 'longitude'=>null, 'latitude'=>null, 'error'=>null, 'note'=>'',  'title'=>array());
+    if (empty($lang)) {
+        $lang = current_language();
+    }
 
-    if (!empty($CFG->geoip2file) and file_exists($CFG->geoip2file)) {
+    $info = [
+        'city' => null,
+        'region' => null,
+        'country' => null,
+        'longitude' => null,
+        'latitude' => null,
+        'error' => null,
+        'note' => '',
+    ];
+
+    if (!\core\ip_utils::is_ip_address($ip)) {
+        $info['error'] = get_string('invalidipformat', 'error');
+        return $info;
+    }
+    if (!ip_is_public($ip)) {
+        $info['error'] = get_string('iplookupprivate', 'error');
+        return $info;
+    }
+
+    if (!empty($CFG->geoip2file) && file_exists($CFG->geoip2file)) {
         $reader = new GeoIp2\Database\Reader($CFG->geoip2file);
-        $record = $reader->city($ip);
+
+        try {
+            $record = $reader->city($ip);
+        } catch (Exception $e) {
+            $info['error'] = $e->getMessage();
+            return $info;
+        }
+
+        /**
+         * Where possible use a localized place names
+         *
+         * @param stdClass $record
+         * @param string $field
+         * @param string $lang which language to extract
+         * @return string local name
+         */
+        function get_translated_name($record, string $field, string $lang): string {
+            if (!empty($record->{$field}->names[$lang])) {
+                return $record->{$field}->names[$lang];
+            }
+            return $record->{$field}->name;
+        }
 
         if (empty($record)) {
             $info['error'] = get_string('iplookupfailed', 'error', $ip);
             return $info;
         }
 
-        $info['city'] = core_text::convert($record->city->name, 'iso-8859-1', 'utf-8');
-        $info['title'][] = $info['city'];
+        $info['city'] = get_translated_name($record, 'city', $lang);
+        $info['region'] = get_translated_name($record, 'mostSpecificSubdivision', $lang);
+        $info['country'] = get_translated_name($record, 'country', $lang);
 
         $countrycode = $record->country->isoCode;
+        $info['countrycode'] = $countrycode;
+
         $countries = get_string_manager()->get_list_of_countries(true);
         if (isset($countries[$countrycode])) {
             // Prefer our localized country names.
             $info['country'] = $countries[$countrycode];
-        } else {
-            $info['country'] = $record->country->names['en'];
         }
-        $info['title'][] = $info['country'];
 
         $info['longitude'] = $record->location->longitude;
         $info['latitude'] = $record->location->latitude;
         $info['note'] = get_string('iplookupmaxmindnote', 'admin');
 
         return $info;
-
     } else if (!empty($CFG->geopluginapikey)) {
-        require_once($CFG->libdir.'/filelib.php');
+        require_once($CFG->libdir . '/filelib.php');
 
         if (strpos($ip, ':') !== false) {
             // IPv6 is not supported by geoplugin.net.
@@ -86,27 +129,25 @@ function iplookup_find_location($ip) {
         }
         $info['latitude'] = (float)$ipdata['geoplugin_latitude'];
         $info['longitude'] = (float)$ipdata['geoplugin_longitude'];
+
         $info['city'] = s($ipdata['geoplugin_city']);
+        $info['region'] = s($ipdata['geoplugin_region']);
         $info['accuracyRadius'] = (int)$ipdata['geoplugin_locationAccuracyRadius']; // Unit is in Miles.
 
         $countrycode = $ipdata['geoplugin_countryCode'];
+        $info['countrycode'] = $countrycode;
         $countries = get_string_manager()->get_list_of_countries(true);
         if (isset($countries[$countrycode])) {
-            // prefer our localized country names
+            // Prefer our localized country names.
             $info['country'] = $countries[$countrycode];
         } else {
             $info['country'] = s($ipdata['geoplugin_countryName']);
         }
 
         $info['note'] = get_string('iplookupgeoplugin', 'admin');
-
-        $info['title'][] = $info['city'];
-        $info['title'][] = $info['country'];
-
         return $info;
     }
 
     $info['error'] = get_string('iplookupfailed', 'error', $ip);
     return $info;
-
 }
